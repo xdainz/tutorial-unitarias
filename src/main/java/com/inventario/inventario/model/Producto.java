87925d501042f92fc6cefe780f12cbdaa4ec9b4f
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-06 00:14:16 -0400

    quitar constructor vacio

--- src/main/java/com/inventario/inventario/model/Producto.java
+++ src/main/java/com/inventario/inventario/model/Producto.java
@@ -2,11 +2,9 @@ package com.inventario.inventario.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 
 public class Producto {
     private int id;
