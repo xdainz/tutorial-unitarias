19ce0beded8b87ed514900494694965366812abd
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-13 18:07:36 -0400

    agregar tag @Repository

--- src/main/java/com/inventario/inventario/repository/ProductoRepository.java
+++ src/main/java/com/inventario/inventario/repository/ProductoRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.inventario.inventario.model.Producto;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
 
     // CRUD
