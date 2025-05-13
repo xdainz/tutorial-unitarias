5eca746a0320470afa0857b9909a64927ae43d0e
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-12 23:43:50 -0400

    agregar integracion con bases de datos

--- src/main/java/com/inventario/inventario/model/Producto.java
+++ src/main/java/com/inventario/inventario/model/Producto.java
@@ -1,15 +1,35 @@
 package com.inventario.inventario.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "producto")
 
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    
+    @Column(length = 100, nullable = false, unique = true)
     private String nombre;
+
+    @Column(length = 15, nullable = false)
     private int precio;
+
+    @Column(length = 10, nullable = false)
     private int stock;
+
+    @Column(length = 100, nullable = false)
     private String marca;
 }
