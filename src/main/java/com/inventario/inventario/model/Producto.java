0c4f40d333cfdf288439ebbb2867072700656ee9
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-13 00:47:54 -0400

    cambiar id a Long

--- src/main/java/com/inventario/inventario/model/Producto.java
+++ src/main/java/com/inventario/inventario/model/Producto.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     
     @Column(length = 100, nullable = false, unique = true)
     private String nombre;
