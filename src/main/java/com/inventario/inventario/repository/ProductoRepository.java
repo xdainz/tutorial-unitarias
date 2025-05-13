d64916f4410f5d5564064a4e0c2c763b89a925da
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-12 23:09:11 -0400

    eliminar datos de prueba

--- src/main/java/com/inventario/inventario/repository/ProductoRepository.java
+++ src/main/java/com/inventario/inventario/repository/ProductoRepository.java
@@ -11,14 +11,6 @@ import com.inventario.inventario.model.Producto;
 public class ProductoRepository {
     private List<Producto> productos = new ArrayList<>();
 
-    ProductoRepository() {
-        productos.add(new Producto(1, "Laptop Gamer", 490000, 15, null));
-        productos.add(new Producto(2, "Smartphone Pro", 323000, 30, null));
-        productos.add(new Producto(3, "Auriculares Inalámbricos", 69990, 50, null));
-        productos.add(new Producto(4, "Tablet 10 pulgadas", 199990, 25, null));
-        productos.add(new Producto(5, "Teclado Mecánico RGB", 60000, 40, null));
-    }
-
     public Producto create(Producto producto) {
         productos.add(producto);
         System.out.println(productos.toString());
