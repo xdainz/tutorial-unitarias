5eb48a2503e124fd5c540c12bf37d2db2a5e52b6
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-13 15:04:00 -0400

    refactorizacion CRUD

--- src/main/java/com/inventario/inventario/repository/ProductoRepository.java
+++ src/main/java/com/inventario/inventario/repository/ProductoRepository.java
@@ -9,10 +9,24 @@ import com.inventario.inventario.model.Producto;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
 
+    // CRUD
+
+    // Create
+        // El metodo save viene creado por defecto y no hace falta implementarlo
+        // El metodo saveAll guarda la lista de productos ingresados
+    
+    // Read
     List<Producto> findAll();
 
-    // "Optional" ayuda cuando el retorno puede ser nulo
-    Optional<Producto> findById(Long id);
+    Optional<Producto> findById(Long id); // "Optional" ayuda cuando el retorno puede ser nulo
+
+    List<Producto> findAllById(List<Long> ids); // retorna una lista con los productos q esten en la lista ingresada
+
+    // Update
+
+    // Delete
+    
+    
 
     boolean existsById(Long id);
 
