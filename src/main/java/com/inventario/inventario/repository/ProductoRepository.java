c81938ec228547316442a3a4193324e47ded83f4
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-14 14:37:15 -0400

    eliminar imports no utilizados

--- src/main/java/com/inventario/inventario/repository/ProductoRepository.java
+++ src/main/java/com/inventario/inventario/repository/ProductoRepository.java
@@ -1,8 +1,5 @@
 package com.inventario.inventario.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +8,4 @@ import com.inventario.inventario.model.Producto;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
 // los metodos on heredados de JpaRepository
-
 }
